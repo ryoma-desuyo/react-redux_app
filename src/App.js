4ba3e365d77f8d536461a928566c45e06bf3546f
ryoma-desuyo <relicsjcbass4@gmail.com>
2019-11-24 12:47:17 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
-function App() {
-  return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log("I am Clicked.")}} />
-    </React.Fragment>
-  )
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() => {console.log("I am Clicked.")}} />
+//     </React.Fragment>
+//   )
 
+// }
+
+const App = () => {
+  return(
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  ) 
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
